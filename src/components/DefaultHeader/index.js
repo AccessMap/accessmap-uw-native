1cc1087c59d7d3607ba5f418bdf7e3db6797a4af
Sundipta Rao <sundipta@gmail.com>
2018-07-03 22:27:52 -0700

    Fixed title alignment issues

--- src/components/DefaultHeader/index.js
+++ src/components/DefaultHeader/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {
   Body,
   Left,
+  Right,
   Title,
 } from 'native-base';
 
@@ -10,12 +11,15 @@ export default class DefaultHeader extends Component<Props> {
   render() {
     return (
       <React.Fragment>
-        <Left>
-          {this.props.left}
-        </Left>
-        <Body>
+        <Left>{this.props.left}</Left>
+        <Body style={{ 
+          flex: 3,
+          justifyContent: 'center', 
+          alignItems: 'center', 
+        }}>
           <Title>AccessMap UW</Title>
         </Body>
+        <Right />
       </React.Fragment>
     );
   }
